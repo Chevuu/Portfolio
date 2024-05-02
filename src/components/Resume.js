cdedaaeaa9df6da85e8be03c5fec32d2562b8d26
Chevuu <vukjurisic@gmail.com>
2024-05-02 12:41:46 +0200

    Refactoring and reconfiguration

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/My_Current_CV.pdf";
+import Particle from "./Particle";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
+import pdf from "../Assets/CV.pdf"
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
